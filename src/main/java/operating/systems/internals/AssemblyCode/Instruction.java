bc36dc052c874c85773c0d6f0d8c1d69f3304b84
Roody <roody.audain@gmail.com>
2016-08-07 11:03:59 -0400

    Deleted?

--- src/main/java/operating/systems/internals/AssemblyCode/Instruction.java
+++ src/main/java/operating/systems/internals/AssemblyCode/Instruction.java
@@ -8,8 +8,7 @@ public class Instruction {
 	/*
 	 * Decode the first word of the instruction into opcode, mode and general
 	 * purpose register using integer division and modulo operators. Example
-	 * machine code: operation_code | operand_1_mode | R | M | R | 5 | 4 | 3 | 2
-	 * | 1
+	 * machine code: operation_code | operand_1_mode | R | M | R | 5 | 4 | 3 | 2 | 1
 	 */
 	public Instruction(int rawInstruction) {
 
