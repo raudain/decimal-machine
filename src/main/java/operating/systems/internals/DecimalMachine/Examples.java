ff44f7fe471c71aa4046d3dc9e50822cd5747b5f
Roody <roody.audain@gmail.com>
2019-11-27 07:33:47 -0500

    Update Examples.java

--- src/main/java/operating/systems/internals/DecimalMachine/Examples.java
+++ src/main/java/operating/systems/internals/DecimalMachine/Examples.java
@@ -1,8 +1,14 @@
 package operating.systems.internals.DecimalMachine;
 
+/**
+ * @author Gayle Laakmann Mcdowell
+ */
 public class Examples {
 
-	
+	/**
+	 * Example 1 <br>
+	 * O(N)
+	 */
 	void product(int[] array) {
 		int sum = 0;
 		int product = 1;
@@ -13,6 +19,10 @@ public class Examples {
 	}
 	
 	
+	/**
+	 * Example 2 <br>
+	 * O(N^2)
+	 */
 	void printPairs(int[] array) {
 		for (int i = 0; i < array.length; i++) {
 			for (int j = 0; j < array.length; j++) {
@@ -22,6 +32,10 @@ public class Examples {
 	}
 	
 	
+	/**
+	 * Example 3 <br>
+	 * O(N^2)
+	 */
 	void printUnorderedPairs(int[] array) {
 		for (int i = 0; i < array.length; i++) {
 			for (int j = i + 1; j < array.length; j++) {
@@ -31,6 +45,10 @@ public class Examples {
 	}
 	
 	
+	/**
+	 * Example 4 <br>
+	 * O(ab)
+	 */
 	void printUnorderedPairs(int[] arrayA, int[] arrayB) {
 		for (int i = 0; i < arrayA.length; i++ ) {
 			for (int j = 0; j < arrayB.length; j++ ) {
@@ -42,6 +60,10 @@ public class Examples {
 	}
 	
 	
+	/**
+	 * Example 5 <br>
+	 * O(ab)
+	 */
 	void Example5(int[] arrayA, int[] arrayB) {
 		for (int i = 0; i < arrayA.length; i++ ) {
 			for (int j = 0; j < arrayB.length; j++ ) {
@@ -52,7 +74,10 @@ public class Examples {
 		}
 	}
 	
-	
+	/**
+	 * Example 6 <br>
+	 * O(N)
+	 */
 	void reverse(int[] array) {
 		for (int i = 0; i < array.length / 2; i++) {
 			int other = array.length - i - 1;
@@ -63,14 +88,10 @@ public class Examples {
 	}
 	
 	
-	/*
-	 * Example 8
-	 * 
-	 * Algorithm where the parameter is an array of strings. The each string is sorted then the array is sorted
-	 * 
+	/**
+	 * Example 9 <br>
+	 * O(N)
 	 */
-	
-	
 	public int sum(Node node) {
 		if (node == null) {
 			return 0;
@@ -85,6 +106,10 @@ public class Examples {
 	}
 	
 	
+	/**
+	 * Example 10 <br>
+	 * O(sqrt(N))
+	 */
 	boolean isPrime(int n) {
 		for (int x = 2; x * x <= n; x++) {
 			if (n % x == 0) {
@@ -95,6 +120,10 @@ public class Examples {
 	}
 	
 	
+	/**
+	 * Example 11 <br>
+	 * O(N)
+	 */
 	int factorial(int n) {
 		if (n < 0) {
 			return -1;
@@ -106,6 +135,10 @@ public class Examples {
 	}
 	
 	
+	/**
+	 * Example 12 <br>
+	 * O(n^2 * n!)
+	 */
 	void permutation(String str) {
 		permutation(str, "");
 	}
@@ -122,17 +155,63 @@ public class Examples {
 	}
 	
 	
-	void allFib(int n) {
-		for (int i = 0; i < n; i++) {
-			System.out.println(i + ": " + fib(i));
-		}
-	}
-	
+	/**
+	 * Example 13 <br>
+	 * O(2^N)
+	 */
 	int fib(int n) {
 		if (n <= 0) return 0;
 		else if (n == 1) return 1;
 		return fib(n-1) + fib(n - 2);
 	}
 	
+	/**
+	 * Example 14 <br>
+	 * O(2^N)
+	 */
+	void allFib(int n) {
+		for (int i = 0; i < n; i++) {
+			System.out.println(i + ": " + fib(i));
+		}
+	}
 	
+	
+	/**
+	 * Example 15 <br>
+	 * O(N)
+	 */
+	void Example15(int n) {
+		int[] memo = new int[n + 1];
+		for (int i = 0; i < n; i++) {
+			System.out.println(i + ": " + fib(i, memo));
+		}
+	}
+	
+	int fib(int n, int[] memo) {
+		if (n <= 0) return 0;
+		else if (n == 1) return 1;
+		else if (memo[n] > 0) return memo[n];
+		
+		memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+		return memo[n];
+	}
+	
+	
+	/**
+	 *  Example 16 <br>
+	 *  O(log n)
+	 */
+	int powersOf2(int n) {
+		if (n < 1) {
+			return 0;
+		} else if (n == 1) {
+			System.out.println(1);
+			return 1;
+		} else {
+			int prev = powersOf2(n / 2);
+			int curr = prev * 2;
+			System.out.println(curr);
+			return curr;
+		}
+	}
 }
